cecc8c94a6e4942221a01cfc63e16fcab414d247
Graeme Read <graeme@sigma957.net>
2024-11-27 19:10:47 +0000

    Concurrent connections (EJ5)

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,21 @@ mod connection;
 mod http;
 mod request;
 mod response;
+mod threadpool;
 
 use connection::Connection;
+use threadpool::ThreadPool;
 
 #[cfg_attr(coverage_nightly, coverage(off))]
 fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221")?;
+    let pool = ThreadPool::new(4);
 
     loop {
         let (stream, _) = listener.accept()?;
         let mut connection = Connection::new(stream);
-        connection.process()?;
+        pool.execute(move || {
+            let _ = connection.process();
+        });
     }
 }
